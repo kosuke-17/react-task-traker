ec0808609165215e93444fbb5e648565ec6dc760
kosuke-17 <tamutamu17@icloud.com>
2021-05-19 21:29:11 +0900

    navとサイドバー修正

--- src/components/Sidebar/SIdebar.js
+++ src/components/Sidebar/SIdebar.js
@@ -1,7 +1,7 @@
 import './Sidebar.css';
 import { Link } from 'react-router-dom';
 
-const Sidebar = ({ show }) => {
+const Sidebar = ({ show, click }) => {
   const sideBarClass = ["sidebar"];
 
   if (show) {
@@ -12,13 +12,12 @@ const Sidebar = ({ show }) => {
 
   return (
     <div className={sideBarClass.join(" ")}>
-      <ul className="">
+      <ul className="sidebar--links" onClick={click}>
         <li>
           <Link to="/notes">メモページへ</Link> 
           <Link to="/notepost">メモ詳細へ</Link> 
         </li>
       </ul>
-      
     </div>
   )
 }
